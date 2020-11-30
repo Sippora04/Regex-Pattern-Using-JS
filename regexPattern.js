8fa0d509c0d80951be603b3c7b7ad18b388c7ac6
Sippora04 <sippora.toppo95@gmail.com>
2020-11-30 20:53:28 +0530

    Modified: handling optional part

--- regexPattern.js
+++ regexPattern.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 
 const PINCODE_REGEX_PATTERN = RegExp('^[1-9]{1}[0-9]{5}|[1-9]{1}[0-9]{2}\\s[0-9]{3}');
-const EMAIL_REGEX_PATTERN = RegExp("^[a-zA-Z0-9]+@[a-zA-Z0-9]+(?:.[a-zA-Z0-9]+)$");
+const EMAIL_REGEX_PATTERN = RegExp('[a-zA-Z0-9]+([._+-][0-9a-zA-Z])*@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
 
 // UC1: Create a regex pattern to validate PIN code 400088.
 // UC2: Create a regex pattern to validate PIN code A400088.
@@ -15,9 +15,10 @@ try{
     console.log(e);
 }
 
-//UC1: Validate Email address with a regex.
-//UC2: Ensure @ and validate the mandatory 2nd part i.e. bridgelabz
-//UC3: Ensure “.” after bridgelabz and validate the mandatory 3rd part i.e. co
+// UC1: Validate Email address with a regex.
+// UC2: Ensure @ and validate the mandatory 2nd part i.e. bridgelabz
+// UC3: Ensure “.” after bridgelabz and validate the mandatory 3rd part i.e. co
+// UC4: Lets handle optional part i.e. xyz in abc.xyz@bridgelabz.co.in
 var email = prompt('Enter Email Id: ');
 try{
     if(!EMAIL_REGEX_PATTERN.test(email)) throw "Invalid email"
