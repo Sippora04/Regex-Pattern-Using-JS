71a95ff49d68753b320f45d564b67973d8ad2924
Sippora04 <sippora.toppo95@gmail.com>
2020-11-30 18:21:08 +0530

    Added: validate email Id

--- regexPattern.js
+++ regexPattern.js
@@ -1,13 +1,20 @@
-// Create a regex pattern to validate PIN code 400088.
-
 const prompt = require('prompt-sync')();
+
 const PINCODE_REGEX_PATTERN = RegExp("^[0-9]{6}$");
-let str = prompt("Enter pincode: ");
+const EMAIL_REGEX_PATTERN = RegExp("^[a-zA-Z0-9]{3}$");
+// Create a regex pattern to validate PIN code 400088.
+var pincode = parseInt(prompt("Enter pincode: "));
 try{
-    if(!PINCODE_REGEX_PATTERN.test(str)) throw "Invalid pincode"
-    {
+    if(!PINCODE_REGEX_PATTERN.test(pincode)) throw "Invalid pincode"
         console.log("Valid pincode");
-    }
+}catch(e){
+    console.log(e);
+}
+//Validate Email address with a regex.
+var email = prompt('Enter Email Here: ');
+try{
+    if(!EMAIL_REGEX_PATTERN.test(email)) throw "Invalid email"
+        console.log("Valid email");
 }catch(e){
     console.log(e);
 }
